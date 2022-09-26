4c7f38af126a61028205da1f3a879dcad29d3f4a
RAT <ricardoalvial@anschile.com>
2022-09-26 18:19:39 -0300

    build args

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ IMAGE="$INPUT_REGISTRY/$INPUT_REPOSITORY/$INPUT_IMAGE_NAME:$INPUT_IMAGE_TAG"
 IMAGE_LATEST="$INPUT_REGISTRY/$INPUT_REPOSITORY/$INPUT_IMAGE_NAME:latest"
 az login --service-principal -u $INPUT_USERNAME -p $INPUT_PASSWORD --tenant $INPUT_TENANT
 az acr login --name $INPUT_REGISTRY
-az acr build -t $IMAGE -t $IMAGE_LATEST  -r ansregistry .
+az acr build -t $IMAGE -t $IMAGE_LATEST  -r ansregistry $INPUT_BUILD_ARGS .
 echo "::set-output name=image_path::$IMAGE"
